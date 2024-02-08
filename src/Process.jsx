3753a810bc683ac110f908501a13168bf190e276
Vinayak S <vinayaksukhalal@gmail.com>
2024-02-08 11:36:16 +0530

    Updated

--- src/Process.jsx
+++ src/Process.jsx
@@ -1,87 +1,83 @@
-import React,{useEffect,useState} from 'react'
-import img from './vinu.jpg'
-import FileUpload from './FileUpload';
-import axios from 'axios';
-import { uploadBytes, ref, listAll, getDownloadURL } from 'firebase/storage';
+import React, { useEffect, useState } from 'react';
+import img from './vinu.jpg';
+import { listAll, getDownloadURL, ref } from 'firebase/storage';
 import { imageDb } from './firebase';
 import * as faceapi from 'face-api.js';
 
 function Process() {
-    const [imageUrl, setImageUrl] = useState([]);
+  const [imageUrl, setImageUrl] = useState([]);
 
-    useEffect(()=>{
-        listAll(ref(imageDb,"files")).then(img=>{
-        console.log(img)
-        img.items.forEach(val=>{
-          getDownloadURL(val).then(url=>{
-            setImageUrl(data=>[...data,url])
-          })
+  useEffect(() => {
+    listAll(ref(imageDb, 'files'))
+      .then((img) => {
+        console.log(img);
+        img.items.forEach((val) => {
+          getDownloadURL(val).then((url) => {
+            setImageUrl((data) => [...data, url]);
+          });
+        });
+      })
+      .catch((error) => {
+        console.error('Error listing files:', error);
+      });
+  }, []);
 
-        })
-        })
-       
-          },[])
-
-          const loadModels = async () => {
-            await Promise.all([
-              faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-              faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-              faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-              faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-              handleChange()
-            ]);
-     
-            
-          };
-          loadModels()
-
-
-    async function handleChange(){
-
-        async function getFaceDescriptor(img) {
-            const detection = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-            if (detection) {
-              return detection.descriptor;
-            }
-           
-          }
-
-          async function findSimilarFace(targetImagePath, groupImagePaths) {
-            // Load the face-api.js models
-          
-            // Load the target facial image
-            const targetImg = await faceapi.fetchImage(targetImagePath);
-            const targetDescriptor = await getFaceDescriptor(targetImg);
-          
-            // Find the most similar image in the group
-            let bestMatchImage;
-            let bestMatchDistance = Number.MAX_VALUE;
-          
-            for (const imagePath of groupImagePaths) {
-              const img = await faceapi.fetchImage(imagePath);
-              const descriptor = await getFaceDescriptor(img);
-          
-              const distance = faceapi.euclideanDistance(targetDescriptor, descriptor);
-              console.log(`Distance for ${imagePath}: ${distance}`);
-          
-              if (distance < bestMatchDistance) {
-                bestMatchDistance = distance;
-                bestMatchImage = imagePath;
-              }
-            }
-          
-            console.log(`Best match image: ${bestMatchImage}`);
-          }
-          const targetImagePath = 'https://avatars.githubusercontent.com/u/103236060?v=4';
-          const groupImagePaths =imageUrl
-          
-    
-          findSimilarFace(targetImagePath, groupImagePaths);
-
+  const loadModels = async () => {
+    try {
+      await Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      ]);
+      handleChange(); // Call handleChange after models are loaded
+    } catch (error) {
+      console.error('Error loading models:', error);
     }
-  return (
-    <button onClick={handleChange}>Process</button>
-  )
+  };
+
+  useEffect(() => {
+    loadModels();
+  }, []);
+
+  async function handleChange() {
+    const targetImagePath = img;
+    const groupImagePaths = imageUrl;
+
+    async function getFaceDescriptor(img) {
+      const detection = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+      if (detection) {
+        return detection.descriptor;
+      }
+    }
+
+    async function findSimilarFace(targetImagePath, groupImagePaths) {
+      const targetImg = await faceapi.fetchImage(targetImagePath);
+      const targetDescriptor = await getFaceDescriptor(targetImg);
+
+      let bestMatchImage;
+      let bestMatchDistance = Number.MAX_VALUE;
+
+      for (const imagePath of groupImagePaths) {
+        const img = await faceapi.fetchImage(imagePath);
+        const descriptor = await getFaceDescriptor(img);
+
+        const distance = faceapi.euclideanDistance(targetDescriptor, descriptor);
+        console.log(`Distance for ${imagePath}: ${distance}`);
+
+        if (distance < bestMatchDistance) {
+          bestMatchDistance = distance;
+          bestMatchImage = imagePath;
+        }
+      }
+
+      console.log(`Best match image: ${bestMatchImage}`);
+    }
+
+    findSimilarFace(targetImagePath, groupImagePaths);
+  }
+
+  return <button onClick={handleChange}>Process</button>;
 }
 
-export default Process
+export default Process;
