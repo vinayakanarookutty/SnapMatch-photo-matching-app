3408eaf69f94a84a3531841dcd25db3f91ad8a97
Vinayak S <vinayaksukhalal@gmail.com>
2024-04-21 20:41:07 +0530

    Updated

--- src/Photo.jsx
+++ src/Photo.jsx
@@ -116,6 +116,7 @@ const Photo = () => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2">
           Capture Photo
         </button>
+       
         {
           isLoading &&(<CircularProgress />)
         }
